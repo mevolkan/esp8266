5d085b02916e1a4f64c03bf603dd98cc4b776f7d
mevolkan <nzaro19@gmail.com>
2019-09-09 07:44:26 +0300

    let's do this

---
+++ esp82866_web_server.ino
@@ -0,0 +1,216 @@
+// ESP8266 webserver
+
+#include <Wire.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h" 
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "DHT.h"
+
+// Replace with your network credentials
+const char* ssid = "AndroidAP_9875";
+const char* password = "Kales237";
+
+// 0X3C+SA0 - 0x3C or 0x3D
+#define I2C_ADDRESS 0x3C
+
+SSD1306AsciiWire oled;
+
+#define DHTPIN 14
+#define DHTTYPE DHT22
+long int dht_millis = 0;
+DHT dht(DHTPIN, DHTTYPE);
+
+
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;    // will store last time DHT was updated
+
+// Updates DHT readings every 10 seconds
+const long interval = 10000;
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>Temperature and Relative Temperature/h2>
+  <br>
+  DHT22
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
+    <span class="dht-labels">Temperature</span> 
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i> 
+    <span class="dht-labels">Humidity</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">%</sup>
+  </p>
+  LM35
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+</script>
+</html>)rawliteral";
+
+// Replaces placeholder with DHT values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return String(t);
+  }
+  else if(var == "HUMIDITY"){
+    return String(h);
+  }
+  return String();
+}
+
+void setup() {
+  //serial port for debugging purposes
+  Serial.begin(115200);
+  //Wire.pins(4, 5);
+  Wire.begin();         
+  oled.begin(&Adafruit128x64, I2C_ADDRESS);
+  oled.set400kHz();  
+  oled.setFont(Adafruit5x7);  
+
+  uint32_t m = micros();
+  oled.clear(); 
+  oled.set2X(); 
+  oled.println("  volkan");
+  oled.set1X();
+  oled.println("Temperature & Humidity\n");
+
+   // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(".");
+  }
+
+  // Print ESP8266 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(t).c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", String(h).c_str());
+  });
+
+  // Start server
+  server.begin();
+
+  dht.begin();
+  delay(1000);
+}
+
+//get temp and humidity from dht11 sensor
+void getTempHumidity()
+{
+  if ((millis() - dht_millis) >= 2000)
+  {
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Check if any reads failed and exit early (to try again).
+    if (!(isnan(h) || isnan(t))) {
+      oled.setCursor(2, 58);
+      oled.print("DHT : "); oled.print(h); oled.print(" | "); oled.print(t);
+      Serial.print("DHT : "); Serial.print(h); Serial.print(" | "); Serial.println(t);
+    }else{
+      Serial.println("No Readings");
+    }
+    dht_millis = millis();
+  }
+}
+
+void tempHumidity(){  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+    // Read temperature as Celsius (the default)
+    float newT = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // if temperature read failed, don't change t value
+    if (isnan(newT)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      t = newT;
+    }
+    // Read Humidity
+    float newH = dht.readHumidity();
+    // if humidity read failed, don't change h value 
+    if (isnan(newH)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      h = newH;
+    }
+  }
+}
+
+
+void loop(void) {
+ getTempHumidity();
+ tempHumidity();
+ delay(10);
+}
